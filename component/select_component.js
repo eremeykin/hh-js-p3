d34361c5d2330fbe2f2a3e3d3ec1765ac342816c
Petr Eremeykin <eremeykin@gmail.com>
2019-01-23 14:44:39 +0300

    Add first implementation of drop down

--- component/select_component.js
+++ component/select_component.js
@@ -1,1 +1,34 @@
-console.log('Not implemented');
+function selectClick(event) {
+    let el = document.getElementsByClassName('select-menu-outer')[0];
+    el.style.display = 'block';
+}
+
+function selectBlur(event){
+    let el = document.getElementsByClassName('select-menu-outer')[0];
+    el.style.display = 'none';
+}
+
+function selectInput(value){
+    fetch('https://api.hh.ru/suggests/areas?text=' + value)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(suggestJson) {
+            console.log(JSON.stringify(suggestJson));
+            let el = document.getElementsByClassName('select-menu')[0];
+            while (el.firstChild) {
+                el.removeChild(el.firstChild);
+            }
+            console.log(suggestJson.items.length);
+            for (let i=0; i<suggestJson.items.length; i++){
+                let option = document.createElement('div');
+                option.className="select-option";
+                console.log(suggestJson.items[i]);
+                option.innerText = suggestJson.items[i].text;
+                el.appendChild(option);
+            }
+        });
+}
+
+console.log('here');
+console.log(selectClick);
